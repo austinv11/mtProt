fc2ff2552b09a926dce6d861e1e08ccec0f029ec
Austin Varela <austinv11@gmail.com>
2022-12-30 15:42:45 -0500

    Only include non-derived features

--- datasets.py
+++ datasets.py
@@ -1,9 +1,6 @@
-from enum import Enum
-
 import pandas as pd
 import pytorch_lightning as pl
 import torch
-from torch import nn
 from torch.utils.data import Dataset, random_split, DataLoader
 
 
@@ -22,21 +19,32 @@ class PandasDataset(Dataset):
 
 class UkBioBankDataModule(pl.LightningDataModule):
 
-    def __init__(self, batch_size: int = 128):
+    def __init__(self, batch_size: int = 128, only_nonderived: bool = True):
         super().__init__()
         self.filepath = "data/BioBank.xlsx"
+        self.meta_filepath = "data/264079_file03.xlsx"  # Supplemental Table S1: https://www.medrxiv.org/content/10.1101/2021.09.24.21264079v2.supplementary-material
         self.batch_size = batch_size
+        self.only_nonderived = only_nonderived
 
     def prepare_data(self):
         df = pd.read_excel(self.filepath, engine="openpyxl",
                            sheet_name="Metabolite Annotations")
-        self.feature_names = df["BIOCHEMICAL"].values.tolist()
+        nightengale_metadata = pd.read_excel(self.meta_filepath, engine="openpyxl",
+                                             sheet_name="Table S1", skiprows=2)
+        nightengale_metadata = nightengale_metadata.drop(columns=["Biomarker", "Units", "Group", "Sub-group", "UKB Field ID", "QC Flag Field ID"])
+        nightengale_metadata = nightengale_metadata.rename(columns={"Description": "BIOCHEMICAL"})
+        df = df.merge(nightengale_metadata, on="BIOCHEMICAL", how="left")
+        self.is_nonderived = (df['Type'] == "Non-derived").values.tolist()
+        if self.only_nonderived:
+            self.feature_names = [name for name, is_nonderived in zip(df["BIOCHEMICAL"].values.tolist(), self.is_nonderived) if is_nonderived]
         self.num_features = len(self.feature_names)
 
     def setup(self, stage=None):
-        if stage == "fit":
+        if stage is None or stage == "fit":
             df = pd.read_excel(self.filepath, engine="openpyxl",
                                sheet_name="Training Set")
+            if self.only_nonderived:
+                df = df[df.columns[self.is_nonderived]]
             datapoints = df.shape[0]
             train_size = int(0.85 * datapoints)
             val_size = datapoints - train_size
@@ -45,6 +53,8 @@ class UkBioBankDataModule(pl.LightningDataModule):
         if stage == "test":
             df = pd.read_excel(self.filepath, engine="openpyxl",
                                sheet_name="Testing Set")
+            if self.only_nonderived:
+                df = df[df.columns[self.is_nonderived]]
             self.test_dataset = PandasDataset(df, df)
 
     def train_dataloader(self):
